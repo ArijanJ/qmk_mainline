600a39755cbb37724fc642d4291b71e2086f06ae
ArijanJ <arijanj@proton.me>
2025-06-21 15:38:25 +0200

    gaming layer

--- keyboards/bastardkb/charybdis/3x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/vendor/keymap.c
@@ -23,6 +23,7 @@
 enum charybdis_keymap_layers {
     LAYER_BASE = 0,
     LAYER_QWERTY,
+    LAYER_GAMING,
     LAYER_NAV,
     LAYER_NUMBER,
     LAYER_BROWSE,
@@ -61,13 +62,17 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define SPC_NUM LT(LAYER_NUMBER, KC_SPC)
 #define LGUI_CBS MT(MOD_LGUI, KC_BSPC)
 #define BROWSE_CBS LT(LAYER_BROWSE, KC_BSPC)
+#define ESC_INT LT(LAYER_INTERNALS, KC_ESC)
+#define ESC_SYM LT(LAYER_SYMBOL, KC_ESC)
 
 enum custom_keycodes {
     FAKE_MOD = SAFE_RANGE,
-    ZOOM_MOD
+    ZOOM_MOD,
+    GAMING_TOGGLE,
 };
 bool fake_mod_active = false;
 bool zoom_mod_active = false;
+bool is_gaming = false;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -91,9 +96,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
           _______,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, _______,
      // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   _______,  KC_SPC,   KC_LALT,      _______,  _______
+                                   _______,  KC_LALT,   KC_LALT,      _______,  _______
      //                            ╰───────────────────────────╯ ╰──────────────────╯
      ),
+
+     [LAYER_GAMING] = LAYOUT(
+          // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+               ESC_SYM, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINUS,
+          // ├───────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────┤
+               KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_BSPC,
+          // ├───────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────┤
+               KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
+        // ╰─────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                            KC_LALT, KC_SPC, KC_TAB,        NAV_ENT, SPC_NUM
+          //                            ╰───────────────────────────╯ ╰──────────────────╯
+          ),
 
      [LAYER_NAV] = LAYOUT(
      // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
@@ -124,8 +141,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
           MO(LAYER_INTERNALS), XXXXXXX, C(KC_W), XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-          KC_LCTL, XXXXXXX, XXXXXXX, MEDIA, XXXXXXX, DF(LAYER_QWERTY),        A(KC_LEFT), C(KC_TAB), C(S(KC_TAB)), A(KC_RGHT), XXXXXXX, XXXXXXX,
-     // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+          KC_LCTL, XXXXXXX, XXXXXXX, MEDIA, XXXXXXX, GAMING_TOGGLE, A(KC_LEFT), C(KC_TAB), C(S(KC_TAB)), A(KC_RGHT), XXXXXXX, XXXXXXX,
+     // ├──────────────────────────────────────────────────────┤ ├───   ───────────────────────────────────────────────────┤
           FAKE_MOD, ZOOM_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      // ╰─────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                         XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______
@@ -146,9 +163,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      [LAYER_SYMBOL] = LAYOUT(
           // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-               _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DF(LAYER_BASE),  XXXXXXX, LSFT(KC_LBRC), KC_EQUAL, LSFT(KC_RBRC), KC_MINUS, KC_SCLN,
+               _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAMING_TOGGLE,  XXXXXXX, LSFT(KC_LBRC), KC_EQUAL, LSFT(KC_RBRC), KC_MINUS, KC_SCLN,
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-               _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LBRC, LSFT(KC_9), KC_BSLS, LSFT(KC_0), KC_RBRC, S(KC_SCLN),
+               _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, GAMING_TOGGLE,  KC_LBRC, LSFT(KC_9), KC_BSLS, LSFT(KC_0), KC_RBRC, S(KC_SCLN),
           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
                _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, KC_MINUS, S(KC_MINUS), KC_GRAVE,
           // ╰─────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
@@ -156,35 +173,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           //                            ╰───────────────────────────╯ ╰──────────────────╯
           ),
 
-     //   [LAYER_LOWER] = LAYOUT(
-     //      // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-     //           XXXXXXX, RGB_TOG, KC_MNXT, KC_MPLY, KC_MPRV, XXXXXXX,    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, XXXXXXX,
-     //      // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-     //           XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PMNS, XXXXXXX,
-     //      // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-     //           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR, QK_BOOT,    KC_PAST,    KC_1,    KC_2,    KC_3, KC_PSLS, XXXXXXX,
-     //      // ╰─────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-     //                                      XXXXXXX, XXXXXXX, _______,    XXXXXXX, _______
-     //      //                            ╰───────────────────────────╯ ╰──────────────────╯
-     //      ),
-
-     //      [LAYER_RAISE] = LAYOUT(
-     //           // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-     //                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX, XXXXXXX,
-     //           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-     //                XXXXXXX, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
-     //           // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-     //                XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-     //           // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-     //                                           _______, _______, XXXXXXX,    _______, XXXXXXX
-     //           //                            ╰───────────────────────────╯ ╰──────────────────╯
-     //           ),
-
      [LAYER_INTERNALS] = LAYOUT(
           // ╭──────────────────────────────────────────────────────╮  ╭──────────────────────────────────────────────────────╮
-               S_D_MOD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+               S_D_MOD,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
           // ├──────────────────────────────────────────────────────┤  ├──────────────────────────────────────────────────────┤
-               S_D_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_RMOD,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+               S_D_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
           // ├──────────────────────────────────────────────────────┤  ├──────────────────────────────────────────────────────┤
                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR, QK_BOOT,        QK_BOOT, EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
           // ╰─────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
@@ -361,6 +354,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     } else if (keycode == ZOOM_MOD) {
         zoom_mod_active = record->event.pressed;
+        return false;
+    }
+
+    if (keycode == GAMING_TOGGLE) {
+        if (record->event.pressed) {
+            layer_off(LAYER_NAV);
+            layer_off(LAYER_INTERNALS);
+            if (is_gaming) {
+                uprintf("process_record_user: deactivating layer LAYER_GAMING\n");
+                set_single_default_layer(LAYER_BASE);
+            } else {
+                uprintf("process_record_user: activating layer LAYER_GAMING\n");
+                set_single_default_layer(LAYER_GAMING);
+            }
+            is_gaming = !is_gaming;
+        }
         return false;
     }
 
